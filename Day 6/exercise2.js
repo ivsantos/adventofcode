7419f0d97ef24ba292553d8dfe72c1079e05eaed
Ivan Santos Panadero <ivan.santos.sge@mango.com>
2021-12-06 13:48:03 +0100

    day 6

---
+++ Day 6/exercise2.js
@@ -0,0 +1,19 @@
+// let fishTimers = [3,4,3,1,2];
+let fishTimers = [1,1,1,1,1,5,1,1,1,5,1,1,3,1,5,1,4,1,5,1,2,5,1,1,1,1,3,1,4,5,1,1,2,1,1,1,2,4,3,2,1,1,2,1,5,4,4,1,4,1,1,1,4,1,3,1,1,1,2,1,1,1,1,1,1,1,5,4,4,2,4,5,2,1,5,3,1,3,3,1,1,5,4,1,1,3,5,1,1,1,4,4,2,4,1,1,4,1,1,2,1,1,1,2,1,5,2,5,1,1,1,4,1,2,1,1,1,2,2,1,3,1,4,4,1,1,3,1,4,1,1,1,2,5,5,1,4,1,4,4,1,4,1,2,4,1,1,4,1,3,4,4,1,1,5,3,1,1,5,1,3,4,2,1,3,1,3,1,1,1,1,1,1,1,1,1,4,5,1,1,1,1,3,1,1,5,1,1,4,1,1,3,1,1,5,2,1,4,4,1,4,1,2,1,1,1,1,2,1,4,1,1,2,5,1,4,4,1,1,1,4,1,1,1,5,3,1,4,1,4,1,1,3,5,3,5,5,5,1,5,1,1,1,1,1,1,1,1,2,3,3,3,3,4,2,1,1,4,5,3,1,1,5,5,1,1,2,1,4,1,3,5,1,1,1,5,2,2,1,4,2,1,1,4,1,3,1,1,1,3,1,5,1,5,1,1,4,1,2,1];
+
+// We now obtain array overflow (larger than 2^32) given this number of breeding fish days.
+const DAYS = 256;
+const fishCount = Array(9).fill(0);
+fishTimers.forEach((timer) => fishCount[timer]++);
+
+for (let i = 0; i < DAYS; i++) {
+  const day0fishes = fishCount[0];
+  for (let j = 0; j < fishCount.length - 1; j++) {
+    fishCount[j] = fishCount[j + 1];
+  }
+  fishCount[6] += day0fishes;
+  fishCount[8] = day0fishes; 
+}
+
+const sum = fishCount.reduce((acc, fish) => acc + fish, 0);
+console.log(`Result: ${sum}`);
